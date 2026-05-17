ad6ce96f5dc145f254f117cb1f17b423e6a73850
Souleymane Dembele <souleymane11dembele@gmail.com>
2026-05-16 19:33:38 -0700

    Added mac address

--- include/configs/socfpga_common.h
+++ include/configs/socfpga_common.h
@@ -246,6 +246,7 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 	"pxefile_addr_r=0x02200000\0" \
 	"ramdisk_addr_r=0x02300000\0" \
 	"socfpga_legacy_reset_compat=1\0" \
+    "ethaddr=ce:7c:70:d2:e3:8d\0" \
 	BOOTENV
 
 #endif
