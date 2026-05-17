2af61cddd8276eba05fc15c2d8cf9dc197c3e143
Souleymane Dembele <souleymane11dembele@gmail.com>
2026-05-16 19:29:10 -0700

    Load FPGA on boot.

--- include/config_distro_bootcmd.h
+++ include/config_distro_bootcmd.h
@@ -489,13 +489,25 @@
 	\
 	BOOT_TARGET_DEVICES(BOOTENV_DEV)                                  \
 	\
-	"distro_bootcmd=" BOOTENV_SET_SCSI_NEED_INIT                      \
-		BOOTENV_SET_NVME_NEED_INIT                                \
-		BOOTENV_SET_IDE_NEED_INIT                                 \
-		BOOTENV_SET_VIRTIO_NEED_INIT                              \
-		"for target in ${boot_targets}; do "                      \
-			"run bootcmd_${target}; "                         \
-		"done\0"
+	"distro_bootcmd= " \
+    "if test -e mmc 0:1 u-boot.scr; then " \
+      "echo --- Found u-boot.scr ---; " \
+      "fatload mmc 0:1 0x2000000 u-boot.scr; " \
+      "source 0x2000000; " \
+    "elif test -e mmc 0:1 gemm_accel_top.rbf; then " \
+      "echo --- Programming FPGA with GEMM Accelerator ---; " \
+      "fatload mmc 0:1 0x2000000 gemm_accel_top.rbf; " \
+      "fpga load 0 0x2000000 0x700000; " \
+    "else " \
+      "echo u-boot.scr and gemm_accel_top.rbf not found in fat.; " \
+    "fi; " \
+    BOOTENV_SET_SCSI_NEED_INIT                      \
+    BOOTENV_SET_NVME_NEED_INIT                                \
+    BOOTENV_SET_IDE_NEED_INIT                                 \
+    BOOTENV_SET_VIRTIO_NEED_INIT                              \
+    "for target in ${boot_targets}; do "                      \
+      "run bootcmd_${target}; "                         \
+    "done\0"
 
 #ifndef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND "run distro_bootcmd"
